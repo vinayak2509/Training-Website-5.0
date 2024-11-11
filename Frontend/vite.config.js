11f637e34705f0051e3f16fc6ffb2f2e8664e5e3
vinayak2509 <vinayak2002590@gmail.com>
2024-11-11 15:09:09 +0530

    .

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,11 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Use the full URL, but make sure it's properly forwarded.
       "/api": {
         target: "https://training-website-5-0-backend.onrender.com",
-        changeOrigin: true, // This ensures the proxy request looks like it's coming from the backend
-        rewrite: (path) => path.replace(/^\/api/, ""), // Remove the "/api" prefix before forwarding
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
