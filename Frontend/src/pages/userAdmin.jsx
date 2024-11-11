5bd5ff659fbc44c99eac980391e727ff3bda4bc4
vinayak2509 <vinayak2002590@gmail.com>
2024-11-11 12:35:01 +0530

    .

--- Frontend/src/pages/userAdmin.jsx
+++ Frontend/src/pages/userAdmin.jsx
@@ -54,13 +54,7 @@ const UserAdmin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/about", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch("http://localhost:5000/api/about");
 
       const data = await response.json();
       if (response.ok) {
